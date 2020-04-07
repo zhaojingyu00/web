4c11fff11cbe798f5dd372fdda3f01a8eb1e15c3
zhaojingyu00 <2819361152@qq.com>
2020-04-07 19:16:16 +0800

    a

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,16 +19,44 @@ module.exports = function(grunt){
         uglify: {
             // my_target:{
               files: {
-                src:'./web.js',
-                dest:'dist/app.js'
+                src:'./*.js',
+                dest:'dist/*.js'
               } 
             // }
-        }
+        },
+        htmlmin:{
+          options:{
+              collapseWhitespace: true,
+              preserveLineBreaks: false
+          },
+          files:{
+              src:'./index.html',
+              dest:'res/index.html'
+          }
+      },
+      cssmin: {  
+          files:{
+              src:'./app.css',
+              dest:'res/app.css'
+          }
+        },
+      uglify: {
+          // my_target:{
+            files: {
+              src:'./*.js',
+              dest:'res/*.js'
+            } 
+          // }
+      }
     });
   
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
   
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-htmlhint');
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
     grunt.registerTask('default',['htmlmin','cssmin','uglify']);
   };
